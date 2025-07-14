0a5a0880f2295d3740fa5ba04cd3e22a94999f3b
rishabh Sinha <rishabhitachi@gmail.com>
2025-07-15 01:18:49 +0530

    Add build.sh for Render TA-lib installation

--- build.sh
+++ build.sh
@@ -2,23 +2,20 @@
 # exit on error
 set -o errexit
 
-# Isse build process saaf dikhega
 echo "Starting TA-Lib Build Process..."
 
 # Step 1: TA-Lib C++ library ko download aur compile karein
 wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
 tar -xzf ta-lib-0.4.0-src.tar.gz
 
-# --- YEH LINE BADLI GAYI HAI ---
-# Hum TA-Lib ko project ke andar hi ek 'ta-lib-bin' folder mein install karenge
-(cd ta-lib && ./configure --prefix=$PWD/../ta-lib-bin && make && make install)
+# Compile aur locally install karein
+(cd ta-lib && ./configure --prefix=/opt/render/project/src/ta-lib-bin && make && make install)
 
 echo "TA-Lib C++ library locally installed successfully."
 
-# Step 2: Pip ko batayein ki TA-Lib kahan rakhi hai
-# Hum environment variable ka istemaal karenge
-export TA_LIBRARY_PATH=$PWD/ta-lib-bin/lib
-export TA_INCLUDE_PATH=$PWD/ta-lib-bin/include
+# Step 2: --- YEH SABSE ZAROORI LINE HAI ---
+# Linker ko batayein ki hamari nayi library kahan rakhi hai
+export LD_LIBRARY_PATH=/opt/render/project/src/ta-lib-bin/lib:$LD_LIBRARY_PATH
 
 # Step 3: Ab Python dependencies install karein
 echo "Installing Python dependencies from requirements.txt..."
