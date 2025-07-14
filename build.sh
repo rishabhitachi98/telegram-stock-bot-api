403f15e0885d18ecf9fff7ff50d24f0e7426c487
rishabh Sinha <rishabhitachi@gmail.com>
2025-07-15 01:02:28 +0530

    Add build.sh for Render TA-lib installation

--- build.sh
+++ build.sh
@@ -1,4 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
 
-apt-get update && apt-get install -y libta-lib-dev
+# TA-Lib ko download aur install karein
+wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
+tar -xzf ta-lib-0.4.0-src.tar.gz
+cd ta-lib/
+./configure --prefix=/usr
+make
+make install
+cd ..
+
+# Ab Python dependencies ko install karein
 pip install -r requirements.txt
