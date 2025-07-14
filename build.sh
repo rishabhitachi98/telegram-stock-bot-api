894c6fbf61d513addb9aa9afdc789cf2746f2746
rishabh Sinha <rishabhitachi@gmail.com>
2025-07-15 01:06:07 +0530

    Add build.sh for Render TA-lib installation

--- build.sh
+++ build.sh
@@ -2,14 +2,21 @@
 # exit on error
 set -o errexit
 
-# TA-Lib ko download aur install karein
+# Isse build process saaf dikhega
+echo "Starting build process..."
+
+# Step 1: TA-Lib C++ library ko download aur compile karein
+echo "Downloading and compiling TA-Lib C++ library..."
 wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
 tar -xzf ta-lib-0.4.0-src.tar.gz
-cd ta-lib/
-./configure --prefix=/usr
-make
-make install
-cd ..
 
-# Ab Python dependencies ko install karein
+# Folder ke andar jaakar compile karein
+(cd ta-lib && ./configure --prefix=/usr && make && make install)
+
+echo "TA-Lib C++ library installed successfully."
+
+# Step 2: Ab Python dependencies install karein
+echo "Installing Python dependencies from requirements.txt..."
 pip install -r requirements.txt
+
+echo "Build process finished successfully!"
